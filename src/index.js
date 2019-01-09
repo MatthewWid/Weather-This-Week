249a7d2db97cb2386a5f0e4427a7a1d8c43b3be7
MatthewWid <115mattey@gmail.com>
2019-01-09 16:25:51 +1100

    create-react-app initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+import config from "./config";
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();

1b807662abe2ba98200eab45f6b2363d844798e4
MatthewWid <115mattey@gmail.com>
2019-01-09 18:15:41 +1100

    Added 'getLocation' helper function for obtaining users' geolocation.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import config from "../../config";
+import getLocation from "../../helpers/getCurrentLocation";
 
 // fetch(`https://api.openweathermap.org/data/2.5/weather?q=sydney,au&appid=${config.API_KEY}`)
 // .then((stream) => {
@@ -12,6 +12,24 @@ import config from "../../config";
 // });
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			location: ""
+		};
+	}
+
+	componentDidMount() {
+		getLocation()
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((res) => {
+				console.warn("Unable to obtain user's geographical location. Continuing without default state.");
+			})
+	}
+
 	render() {
 		return (
 			<div className="App">
--- src/helpers/getCurrentLocation.js
+++ src/helpers/getCurrentLocation.js
@@ -5,6 +5,11 @@ function getLocation() {
 		console.warn("\"geolocation\" not available in user navigator.");
 		return;
 	}
+
+	// https://stackoverflow.com/a/22519785/2954591
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject);
+	});
 }
 
 export default getLocation;

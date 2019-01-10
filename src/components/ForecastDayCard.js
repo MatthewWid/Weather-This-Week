292ced87ac42633d6fc52510c7c9da2654be7f4e
MatthewWid <115mattey@gmail.com>
2019-01-10 18:19:30 +1100

    Added five day forecast with icons and temperature predictions.

--- src/components/ForecastDayCard.js
+++ src/components/ForecastDayCard.js
@@ -1,9 +1,20 @@
 import React, {Component} from "react";
+import "./ForecastDayCard.css";
 
 class ForecastDayCard extends Component {
 	render() {
+		console.log(this.props.weather);
+
 		return (
-			<p>Day Card</p>
+			<div className="weather-forecast-card">
+				<div className={`weather-forecast-card__day${this.props.index === 0 ? " weather-forecast-card__day--underline" : ""}`}>
+					{this.props.dayName}
+				</div>
+				<img src={`http://openweathermap.org/img/w/${this.props.weather.weather[0].icon}.png`} className="weather-forecast-card__image" title={this.props.weather.weather[0].description} alt={this.props.weather.weather[0].description} />
+				<div className="weather-forecast-card__temp">
+					{parseInt(this.props.weather.main.temp)}°C
+				</div>
+			</div>
 		);
 	}
 }
--- src/components/WeekForecast.js
+++ src/components/WeekForecast.js
@@ -1,13 +1,29 @@
 import React, {Component} from "react";
 import ForecastDayCard from "./ForecastDayCard";
+import "./WeekForecast.css";
 
 class WeekInformation extends Component {
 	render() {
-		console.log(this.props);
+		// Get the forecast data from noon for today and the next four days
+		let days = [];
+		this.props.forecast.list.forEach((day) => {
+			if (day.dt_txt.split(" ")[1].split(":")[0] === "12") {
+				days.push(day);
+			}
+		});
+		
+		// const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+		const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+		const cards = days.map((day, i) => {
+			// Get the name of the day by mapping the date text field number value to the dayNames array
+			const dayName = dayNames[(new Date(day.dt_txt)).getDay()];
+
+			return <ForecastDayCard dayName={dayName} weather={day} key={i} index={i} />;
+		});
 
 		return (
 			<section className="weather-forecast">
-				<ForecastDayCard />
+				{cards}
 			</section>
 		);
 	}

4e6f6f7c2d68c278b03fedbee6ad9a69f1881d3e
MatthewWid <115mattey@gmail.com>
2019-01-11 23:09:14 +1100

    Updates

---
+++ precache-manifest.13bd1d5887b4578a7e8e6cd4a10ea1d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "040ecca5fd1b91cb7b83",
+    "url": "/Weather-This-Week/static/js/runtime~main.040ecca5.js"
+  },
+  {
+    "revision": "795b8f9ad8ac7244cd9d",
+    "url": "/Weather-This-Week/static/js/main.795b8f9a.chunk.js"
+  },
+  {
+    "revision": "07140812a98c93f90dd2",
+    "url": "/Weather-This-Week/static/js/1.07140812.chunk.js"
+  },
+  {
+    "revision": "795b8f9ad8ac7244cd9d",
+    "url": "/Weather-This-Week/static/css/main.098259f8.chunk.css"
+  },
+  {
+    "revision": "6b5d0f61cd91463c7bd828f87e1f0e43",
+    "url": "/Weather-This-Week/index.html"
+  }
+];
